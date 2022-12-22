f0a5348fe8f65fadfa342fceefd40e6aaf964c23
rezafikkri <fikkri.reza@gmail.com>
2022-12-22 08:25:11 +0700

    feat: tipe data boolean and string

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Reza")
+  fmt.Println("Reza Sariful")
+  fmt.Println("Reza Sariful Fikri")
+
+  fmt.Println(len("Reza"))
+  fmt.Println("Reza Sariful"[0])
+  fmt.Println("Reza Sariful Fikri"[1])
+}
