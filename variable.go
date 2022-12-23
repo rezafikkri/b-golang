3b5b8de4f74c35e857ad5f0de94cbdc9c2872fbb
rezafikkri <fikkri.reza@gmail.com>
2022-12-23 07:25:14 +0700

    feat variable

---
+++ variable.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+  var name string = "Reza"
+  fmt.Println(name)
+
+  name = "Reza Fikkri"
+  fmt.Println(name)
+
+  name = "Reza Sariful"
+  fmt.Println(name)
+}
